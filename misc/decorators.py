16e11b9402a2de6d0ea9c4f5f79a829ed4371308
Tsuyoshi Aizu <aizu@bascule.co.jp>
2015-10-29 17:48:46 +0900

    retryデコレータを追加

--- misc/decorators.py
+++ misc/decorators.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
+import sys
 from functools import wraps
+from time import sleep
 
 from ojos.misc.exceptions import ResponseException
 
@@ -16,7 +18,7 @@ def error_response(code=500, message=None, header_code=True):
 
             try:
                 return f(_self, *args, **kwargs)
-            except ResponseException, e:
+            except ResponseException as e:
                 if e.url is not None:
                     return _self.redirect(e.url, e.code)
 
@@ -30,10 +32,35 @@ def error_response(code=500, message=None, header_code=True):
                     e.header_code = header_code
 
                 return error(e, *args, **kwargs)
-            except Exception, e:
+            except Exception as e:
                 e.code = code
                 e.message = message
                 e.header_code = header_code
                 return error(e, *args, **kwargs)
+        return callf
+    return decorator
+
+
+# リトライデコレータ
+def retries(max_tries, delay=0, backoff=0, exceptions=(Exception,), hook=None):
+    def decorator(f):
+        @wraps(f)
+        def callf(*args, **kwargs):
+            mydelay = delay
+            tries = range(max_tries)
+            tries.reverse()
+            for tries_remaining in tries:
+                try:
+                    return f(*args, **kwargs)
+                except exceptions as e:
+                    if tries_remaining > 0:
+                        if hook is not None:
+                            hook(tries_remaining, e, mydelay)
+                        sleep(mydelay)
+                        mydelay = mydelay * backoff
+                    else:
+                        raise
+                else:
+                    break
         return callf
     return decorator
